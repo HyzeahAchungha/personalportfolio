6bbe9421d224f8468a1dcc710009ab6f4a03433b
kuku67011 <hyzeala28@gmail.com>
2023-01-09 09:39:16 +0100

    Card,Contact,Experience,FloatingDiv,Footer,Intro,Navbar,Portfolio,Services,Testimonials,Toggle,Works,img,Context

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -39,7 +39,7 @@ const Intro = () => {
         </Link>
         {/* social icons */}
         <div className="i-icons">
-          <img src={Github} alt="" />
+          <a href="https://github.com/HyzeahAchungha"><img src={Github} alt="" /></a>
           <img src={LinkedIn} alt="" />
           <img src={Instagram} alt="" />
         </div>
